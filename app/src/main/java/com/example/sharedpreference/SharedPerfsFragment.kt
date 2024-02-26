9b232109bbdd137745c603472e25b67027c78214
drishti2281 <121477988+drishti2281@users.noreply.github.com>
2024-02-26 15:45:21 +0530

    Changes

--- app/src/main/java/com/example/sharedpreference/SharedPerfsFragment.kt
+++ app/src/main/java/com/example/sharedpreference/SharedPerfsFragment.kt
@@ -8,10 +8,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.sharedpreference.databinding.FragmentSharedPerfsBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,30 +20,24 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SharedPerfsFragment.newInstance] factory method to
+ * Use the [SharedPrefsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SharedPerfsFragment : Fragment() {
+class SharedPrefsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    //Declaration  of binding,Activity,editor,Sharedpreference,int,adapter
-    var binding: FragmentSharedPerfsBinding? =null
+    var binding : FragmentSharedPerfsBinding ? = null
     lateinit var mainActivity: MainActivity
     lateinit var sharedPreferences: SharedPreferences
     lateinit var editor: Editor
+    lateinit var adapter: Recyclerview
     var count: Int = 0
-    var color: Int = 0
-    lateinit var adapter: RecyclerView
-    //0 is use for select the red,green,blue Color in the recycler view
-    var selectColor: Int = 0
-
-
+    var selectedColor: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivity = activity as MainActivity
-
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -54,56 +48,58 @@ class SharedPerfsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentSharedPerfsBinding.inflate(layoutInflater)
-        return (binding?.root)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //initialize
-        adapter= RecyclerView(mainActivity)
-        binding?.recyclerView?.layoutManager = LinearLayoutManager(mainActivity,LinearLayoutManager.VERTICAL,false)
-      //  binding?.recyclerView?.adapter = RecyclerView
-        sharedPreferences = mainActivity.getSharedPreferences(mainActivity.resources.getString(
-            R.string.app_name), MODE_PRIVATE)
-        editor =sharedPreferences.edit()
+        sharedPreferences = mainActivity.getSharedPreferences(mainActivity.getString(R.string.app_name), MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+        adapter = Recyclerview(mainActivity)
 
-        count = sharedPreferences.getInt("Countnumber",0)
-        color = sharedPreferences.getInt("Changecolor",0)
+        binding?.recyclerView?.layoutManager = LinearLayoutManager(mainActivity,LinearLayoutManager.VERTICAL, false)
+        binding?.recyclerView?.adapter = adapter
 
-
+        updateUI()
 
         binding?.btnRed?.setOnClickListener {
-            selectColor = 0
-            if (binding?.etcount?.text?.toString()?.trim().isNullOrEmpty())
-                binding?.etcount?.error = mainActivity.resources.getString(R.string.enter_number)
-
-        }
-        binding?.btnGreen?.setOnClickListener {
-            selectColor = 1
-            if (binding?.etcount?.text?.toString()?.trim().isNullOrEmpty())
-                binding?.etcount?.error = mainActivity.resources.getString(R.string.enter_number)
-
+            selectedColor = 0
         }
         binding?.btnBlue?.setOnClickListener {
-            selectColor = 2
-            if (binding?.etcount?.text?.toString()?.trim().isNullOrEmpty())
-                binding?.etcount?.error = mainActivity.resources.getString(R.string.enter_number)
+            selectedColor = 1
         }
-        binding?.btnSave?.setOnClickListener {
-            if(binding?.etcount?.text?.toString()?.trim().isNullOrEmpty())
-                binding?.etcount?.error = mainActivity.resources.getString(R.string.enter_number)
+        binding?.btnGreen?.setOnClickListener {
+            selectedColor = 2
+        }
 
-            editor.putInt("RollNo", (binding?.etcount?.text?.toString()?:"0").toInt())
-            editor.commit()
-            editor.apply()
+        binding?.btnSave?.setOnClickListener {
+            if (binding?.etcount?.text.toString().isNullOrEmpty()){
+                binding?.etcount?.error = resources.getString(R.string.enter_number)
+            }else{
+                editor.putInt("Count",(binding?.etcount?.text?.toString()?:"0").toInt())
+                editor.putInt("Color",selectedColor)
+                editor.commit()
+                editor.apply()
+                Toast.makeText(requireContext(),resources.getString(R.string.Save),Toast.LENGTH_SHORT).show()
+                updateUI()
+            }
         }
         binding?.btnClear?.setOnClickListener {
             editor.clear()
             editor.apply()
             editor.commit()
+            Toast.makeText(requireContext(),resources.getString(R.string.Clear),Toast.LENGTH_SHORT).show()
+            updateUI()
         }
+    }
+
+    fun updateUI(){
+        count = sharedPreferences.getInt("Count", 0)
+        selectedColor = sharedPreferences.getInt("Color", 0)
+        binding?.etcount?.setText(count.toString())
+        adapter.updateCount(count)
+        adapter.updateColor(selectedColor)
     }
 
     companion object {
@@ -113,12 +109,12 @@ class SharedPerfsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SharedPerfsFragment.
+         * @return A new instance of fragment SharedPrefsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SharedPerfsFragment().apply {
+            SharedPrefsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
